a2b36e459c69ed8688b5ecaf6953e7e02b4aad99
Samba Catalyst <samba@catalyst.net.nz>
2018-11-26 12:30:51 +1300

    adws/xmlutils.py: add file
    Signed-off-by: Samba Catalyst <samba@catalyst.net.nz>

---
+++ adws/xmlutils.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from lxml import etree
+
+NAMESPACES = {
+    "s": "http://www.w3.org/2003/05/soap-envelope",
+    "a": "http://www.w3.org/2005/08/addressing",
+    "addata": "http://schemas.microsoft.com/2008/1/ActiveDirectory/Data",
+    "ad": "http://schemas.microsoft.com/2008/1/ActiveDirectory",
+    "da": "http://schemas.microsoft.com/2006/11/IdentityManagement/DirectoryAccess",
+    "xsd": "http://www.w3.org/2001/XMLSchema",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+}
+
+
+def elem_get_text(elem):
+    if elem is not None:
+        text = elem.text
+        if text is not None:
+            return text.strip()
+    return ''
+
+
+def elem_is_empty(elem):
+    # no text, no children, then empty
+    return len(elem) == 0 and elem_get_text(elem) == ''
+
+
+def elem_tostring(elem, pretty_print=True, encoding='unicode'):
+    """pretty and unicode by default"""
+    return etree.tostring(
+        elem, pretty_print=pretty_print, encoding=encoding)
+
+
+
+def print_xml(xml, sn=0, mode='w+'):
+    # parse to validate
+    root = etree.fromstring(xml)
+    # print('######################XML HEAD##########################')
+    # xml2 = etree.tostring(root, pretty_print=True)
+    # print(xml2)
+    # print('######################XML TAIL##########################')
+    with open('/vagrant/%s.xml' % sn, mode) as f:
+        f.write(xml + '\n\n\n')
+
+
+class XMLHelper(object):
+    """
+    A class helps to extract data from xml.
+    """
+
+    def __init__(self, xml):
+        self.xml = xml
+        self.root = etree.fromstring(xml)
+        self.nsmap = self.root.nsmap
+        # root ns + common ns
+        self.nsmap.update(NAMESPACES)
+
+    def get_elem(self, xpath, as_text=False):
+        elem = self.root.find(xpath, self.nsmap)
+        return elem_get_text(elem) if as_text else elem
+
+    def get_elem_text(self, xpath):
+        return self.get_elem(xpath, as_text=True)
+
+    def get_elem_list(self, xpath, as_text=False):
+        print(self.nsmap)
+        elems = self.root.findall(xpath, self.nsmap)
+        return [elem.text.strip() for elem in elems] if as_text else elems
+
+    def is_elem_empty(self, xpath):
+        """
+        A empty element has no text and children
+
+        e.g.: <s:Body></s:Body>
+        """
+        elem = self.root.find(xpath, self.nsmap)
+        return elem_is_empty(elem)
+
