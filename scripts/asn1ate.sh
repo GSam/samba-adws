e2bd616f62b742b63c92834057919bd448bb00c3
Garming Sam <garming@samba.org>
2024-08-27 09:26:02 +0000

    Finish rename of generated controls

--- scripts/asn1ate.sh
+++ scripts/asn1ate.sh
@@ -1,2 +1,2 @@
 PYTHON_SITE_PACKAGES="/home/garming/.local/lib/python3.10/site-packages/"
-python3 $PYTHON_SITE_PACKAGES/asn1ate/pyasn1gen.py scripts/controls.asn1 > controls.py
+python3 $PYTHON_SITE_PACKAGES/asn1ate/pyasn1gen.py scripts/controls.asn1 > asn1ctrl.py
